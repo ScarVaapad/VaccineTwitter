79d0f7a9c87fcaf457f1c125c7fe7b3ad3a3d17c
ScarVaapad <scarcloudwalker@gmail.com>
2021-08-10 15:00:03 -0400

    Setting up plans for discovering insights for verified users

--- PracticeEDA.py
+++ PracticeEDA.py
@@ -16,17 +16,65 @@ directory = os.path.join('data',filename)
 hydrated = pd.read_csv(directory, dtype='unicode')
 
 # We need to convert all columns into the right data type, maybe remove some unnecessary columns
-hydrated["retweet_count"] = pd.to_numeric(hydrated["retweet_count"], downcast="integer")
-hydrated["favorite_count"] = pd.to_numeric(hydrated["favorite_count"], downcast="integer")
-hydrated["user_listed_count"] = pd.to_numeric(hydrated["user_listed_count"], downcast="integer")
-hydrated["user_statuses_count"] = pd.to_numeric(hydrated["user_statuses _count"], downcast="integer")
-hydrated["user_favorites_count"] = pd.to_numeric(hydrated["user_favourites_count"], downcast="integer")
+hydrated["retweet_count"] = pd.to_numeric(hydrated["retweet_count"], downcast="float")
+hydrated["favorite_count"] = pd.to_numeric(hydrated["favorite_count"], downcast="float")
+hydrated["user_listed_count"] = pd.to_numeric(hydrated["user_listed_count"], downcast="float")
+hydrated["user_statuses_count"] = pd.to_numeric(hydrated["user_statuses _count"], downcast="float")
+hydrated = hydrated.drop(columns=["user_statuses _count"])
+hydrated["user_favourites_count"] = pd.to_numeric(hydrated["user_favourites_count"], downcast="float")
 hydrated["created_at"] = pd.to_datetime(hydrated["created_at"])
 
-# Test Field on a single user (realBenTalks)
+# Clean the dataframe
 testdf = hydrated
-testdf=testdf.drop(columns=['Unnamed: 0','Unnamed: 0.1','Unnamed: 0.1.1','id_str'])
+testdf = testdf.drop(columns=['Unnamed: 0','Unnamed: 0.1','Unnamed: 0.1.1','id_str','from_user_id_str'])
+testdf = testdf.drop(index=testdf[testdf.user_lang.notna()].index)
+testdf = testdf.drop(columns=['user_lang'])
 
+# Tweeter counts over time
+testdf = testdf.sort_values(by=['created_at'])
+weekly_tweet = testdf.resample('w',on='created_at').count()
+weekly_tweet.index = weekly_tweet.index.date
+
+ax = weekly_tweet.plot(kind='bar',y='letter_id_str',figsize=(8,5))
+ax.set_xlabel('Date - Week Starting')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Establish a dataframe based on users
+df_by_user = testdf.groupby('from_user')
+user_dict = {}
+for username, sub_df in df_by_user:
+    sub_df.sort_values('created_at')
+    data = {}
+    data["tweets_count"] = sub_df["text"].count()
+    data["user_created_at"] = pd.to_datetime(sub_df["user_created_at"][sub_df["user_created_at"].index[0]]).date()
+    data["user_verified"] = sub_df["user_verified"].notnull().any()
+    user_dict[username]=data
+
+new_df = pd.DataFrame.from_dict(user_dict,'index')
+new_df.to_csv(os.path.join("data","user_stats.csv"))
+new_df = new_df.sort_values('tweets_count',ascending=False)
+new_df = new_df.sort_values('user_created_at')
+new_df = new_df.sort_values("user_verified")
+new_df.user_created_at = pd.DatetimeIndex(new_df.user_created_at).to_period('M')
+month_group = new_df.groupby('user_created_at')
+
+for month,sub_df in month_group:
+    print(month," --> ",sub_df.user_verified.value_counts(normalize=True)*100)
+
+new_df.value_counts('user_verified')
+
+# To investigate verified individuals:
+# Step 1. Scrapping all user_description and make it a corpus.
+# Step 2. Remove the stop words and count the TFs
+# Step 3. Show stats of word count, choose top 50?
+# Step 4. Arbitrarily define two brackets of words to distinguish between Media and Individuals
+# Step 5. Run it through all verified users
+
+df_verified = testdf[testdf.user_verified == 'TRUE']
+user_des_list = df_verified.user_description.unique().tolist()
+# Test Field on a single user (realBenTalks)
 ben = testdf[testdf.from_user=='realBenTalks']
 ben = ben.sort_values(by=['created_at'])
 
@@ -54,19 +102,19 @@ cnt_true = 0
 cnt_false =0
 cnt_mix = 0
 error = 0
-sumcount = 1000
+sumcount = -1
 for index,data in hydrated.groupby(['from_user']):
     if sumcount ==0 :
         break
     sumcount-=1
     if data.user_verified.value_counts().count()>1:
         cnt_mix+=1
-        print(data.user_verified.value_counts())
-        print(data.user_verified.values)
+        #print(data.user_verified.value_counts())
+       # print(data.user_verified.values)
     elif data.user_verified.values[0]== True:
         cnt_true+=1
-        print(data.user_verified.value_counts())
-        print(data.user_verified.values)
+       # print(data.user_verified.value_counts())
+        #print(data.user_verified.values)
     else:
         cnt_false+=1
 
